bd17359e36b1edc26b4de84f583ed086c835078c
SIDDU BIRADAR <SIDDUBIRADAR581@GMAIL.COM>
2024-11-20 02:28:53 +0530

    JUST AN MINOR CHANGE IN DB CONNECTION

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','SIDDALING#BIRADAR@1','OnlineBookStore_db') or die('connection failed');
+$conn = mysqli_connect('localhost','root','BIRADAR@11','OnlineBookStore_db') or die('connection failed');
 
 ?>
